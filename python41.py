8efdf7291a2391543c86e2ccc86d1c458e5dcd8b
artb433 <tahiruabdulrahmanbaaba3@gmail.com>
2023-09-09 15:07:36 +0000

    Inheritance, polymorphism and magic methods

--- python41.py
+++ python41.py
@@ -1,6 +1,6 @@
-class animal:
+class Animal:
     def __int__(self, birthType="Unknown", appearance="Unknown", blooded="Unknown"):
-        self.birth = birthType
+        self.birthType = birthType
         self.appearance = appearance
         self.blooded = blooded
 
@@ -31,24 +31,47 @@ class animal:
     def __str__(self):
         return "A {} is {} it is {} it is {}".format(type(self).__name__, self.birthType, self.appearance, self.blooded)
 
-    class Mammal(Animal):
-        def __int__(self, birthType="born alive",
-                    appearance="hair or fur",
-                    blooded="warm blooded",
-                    nurseYoung=True):
 
-            Animal.__init__(self,birthType, appearance, blooded)
+class Mammal(Animal):
+    def __int__(self, birthType= "born alive",
+                appearance="hair or fur",
+                blooded="warm blooded",
+                nurseYoung=True):
+        Animal.__init__(self, birthType, appearance, blooded)
 
-            self.__nurseYoung = nurseYoung
+        self.__nurseYoung = nurseYoung
 
-        @property
-        def nurseYoung(self):
-            return self.__nurseYoung
+    @property
+    def nurseYoung(self):
+        return self.__nurseYoung
 
-        @nurseYoung.setter
-        def nurseYoung(self, nurseYoung):
-            self.__nurseYoung = nurseYoung
+    @nurseYoung.setter
+    def nurseYoung(self, nurseYoung):
+        self.__nurseYoung = nurseYoung
 
-        def __str__(self):
-            return super().__str__() + " and it is {} they their young".format(self.nurseYoung)
+    def __str__(self):
+        return super().__str__() + " and it is {} they their young".format(self.nurseYoung)
+
+
+class Reptile(Animal):
+    def __int__(self, birthType="born in an egg or born alive",
+                appearance="dry scales",
+                blooded="cold blooded"):
+        Animal.__init__(self, birthType, appearance, blooded)
+
+    def sumAll(self, *args):
+        sum = 0
+
+        for i in args:
+            sum += i
+        return sum
+
+
+def main():
+
+    animal1 = Animal("born alive")
+    print(animal1.birthType)
+
+
+main()
 
