0bda9e9c2096af845a0b58e01e59b4086809fa83
artb433 <tahiruabdulrahmanbaaba3@gmail.com>
2023-09-09 14:48:44 +0000

    Magic methods

--- python41.py
+++ python41.py
@@ -27,3 +27,28 @@ class animal:
     @blooded.setter
     def blooded(self, blooded):
         self.__blooded = blooded
+
+    def __str__(self):
+        return "A {} is {} it is {} it is {}".format(type(self).__name__, self.birthType, self.appearance, self.blooded)
+
+    class Mammal(Animal):
+        def __int__(self, birthType="born alive",
+                    appearance="hair or fur",
+                    blooded="warm blooded",
+                    nurseYoung=True):
+
+            Animal.__init__(self,birthType, appearance, blooded)
+
+            self.__nurseYoung = nurseYoung
+
+        @property
+        def nurseYoung(self):
+            return self.__nurseYoung
+
+        @nurseYoung.setter
+        def nurseYoung(self, nurseYoung):
+            self.__nurseYoung = nurseYoung
+
+        def __str__(self):
+            return super().__str__() + " and it is {} they their young".format(self.nurseYoung)
+
